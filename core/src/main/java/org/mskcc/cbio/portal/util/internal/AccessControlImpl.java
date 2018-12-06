0170a892a0b8ef542c12cade1b44b274636ea822
Angelica Ochoa <aochoa4230@gmail.com>
2018-12-06 16:22:10 -0500

    Remove unused authorization portal property
    Signed-off-by: Angelica Ochoa <aochoa4230@gmail.com>

--- core/src/main/java/org/mskcc/cbio/portal/util/internal/AccessControlImpl.java
+++ core/src/main/java/org/mskcc/cbio/portal/util/internal/AccessControlImpl.java
@@ -84,7 +84,7 @@ public class AccessControlImpl implements AccessControl {
 
         //  Then, insert "All" Cancer Types at beginning  //TODO - fix this! It conflicts with ALL (Acute Lymphoid Leukemia)!
         ArrayList<CancerStudy> finalCancerStudiesList = new ArrayList<CancerStudy>();
-        String allCancerStudyTitle = (GlobalProperties.usersMustBeAuthorized()) ?
+        String allCancerStudyTitle = (GlobalProperties.usersMustAuthenticate()) ?
             "All Authorized Cancer Studies" : "All Cancer Studies";
         CancerStudy allCancerStudy = new CancerStudy(allCancerStudyTitle, allCancerStudyTitle,
              "all", "all", true);
@@ -131,7 +131,7 @@ public class AccessControlImpl implements AccessControl {
 
     public UserDetails getUserDetails()
     {
-        if (GlobalProperties.usersMustBeAuthorized()) {
+        if (GlobalProperties.usersMustAuthenticate()) {
             Authentication auth = SecurityContextHolder.getContext().getAuthentication();
             if (auth == null) {
                 String errorMessage = "Possible configuration error detected: authorization=true but no authentication found. "
