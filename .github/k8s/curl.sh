52dbd47549aa6f6a11dc8c01b51a28d03bbfd5d3
Manda Wilson <1458628+mandawilson@users.noreply.github.com>
2020-11-11 09:05:13 -0500

    fix if statement

--- .github/k8s/curl.sh
+++ .github/k8s/curl.sh
@@ -20,11 +20,11 @@ if [ "$RESPONSE_CODE" != "$OK_RESPONSE_CODE" ]; then
     echo "Request failed with response code '$RESPONSE_CODE', expected '$OK_RESPONSE_CODE'"
     exit 1
 else
-    if [ "$EXPECT_EMPTY_LIST_RESPONSE" == "1" ] && [ "$RESPONSE_BODY" != "[]" ]; then
+    if [ "$EXPECT_EMPTY_LIST_RESPONSE" -eq 1 ] && [ "$RESPONSE_BODY" != "[]" ]; then
         echo "Request returned a non-empty response when we expect an empty list."
         exit 1
     fi
-    if [ "$EXPECT_EMPTY_RESPONSE" == "1" ] && [ ! -z "$RESPONSE_BODY" ]; then
+    if [ "$EXPECT_EMPTY_RESPONSE" -eq 1 ] && [ ! -z "$RESPONSE_BODY" ]; then
         echo "Request returned a non-empty response when we expect an empty response."
         exit 1
     fi
