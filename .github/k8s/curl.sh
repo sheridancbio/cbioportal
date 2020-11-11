c754a16581f506ad3ace1ec67228b09bb5601985
Manda Wilson <1458628+mandawilson@users.noreply.github.com>
2020-11-11 08:31:20 -0500

    converted rest of curls to script calls

--- .github/k8s/curl.sh
+++ .github/k8s/curl.sh
@@ -1,14 +1,16 @@
 #!/bin/bash
 POD_NAME=$1
-MAX_TIME=$2
+MAX_WAIT_TIME_IN_SECONDS=$2
 URL=$3
 SEARCH_STR=$4
 POST_BODY=$5
+EXPECT_EMPTY_LIST_RESPONSE=$6
+EXPECT_EMPTY_RESPONSE=$7
 
 OK_RESPONSE_CODE=200
 EMPTY_LIST_RESPONSE='[]'
 
-kubectl exec -it --namespace=performance-test $POD_NAME -- bash -c "curl --max-time $MAX_TIME -s -w '\n\n%{http_code}' -X POST '$URL' -H 'accept: application/json' -H 'Content-Type: application/json' -d '$POST_BODY'" > curl.out
+kubectl exec -it --namespace=performance-test $POD_NAME -- bash -c "curl --max-time $MAX_WAIT_TIME_IN_SECONDS -s -w '\n\n%{http_code}' -X POST '$URL' -H 'accept: application/json' -H 'Content-Type: application/json' -d '$POST_BODY'" > curl.out
 RESPONSE_CODE=`tail -1 curl.out`
 RESPONSE_BODY=`head -1 curl.out`
 echo "THIS RESPONSE BODY"
@@ -18,12 +20,20 @@ if [ "$RESPONSE_CODE" != "$OK_RESPONSE_CODE" ]; then
     echo "Request failed with response code '$RESPONSE_CODE', expected '$OK_RESPONSE_CODE'"
     exit 1
 else
+    if [ "$EXPECT_EMPTY_LIST_RESPONSE" == "1" ] && [ "$RESPONSE_BODY" != "[]" ]; then 
+        echo "Request returned a non-empty response when we expect an empty list."
+        exit 1
+    fi
+    if [ "$EXPECT_EMPTY_RESPONSE" == "1" ] && [ ! -z "$RESPONSE_BODY" ]; then
+        echo "Request returned a non-empty response when we expect an empty response."
+        exit 1
+    fi 
     if [ "$RESPONSE_BODY" == "$EMPTY_LIST_RESPONSE" ] || [ -z "$RESPONSE_BODY" ]; then 
-        echo "Request failed with empty response"
+        echo "Request failed with empty response."
         exit 1
     else
         if [[ "$RESPONSE_BODY" != *"$SEARCH_STR"* ]]; then
-           echo "Response did not contain search string '$SEARCH_STR'"
+           echo "Response did not contain search string '$SEARCH_STR'."
             exit 1
         fi
     fi
