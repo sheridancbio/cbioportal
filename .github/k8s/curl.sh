48d9e553d6836a999a3349c669d914ca9e6c921c
Manda Wilson <1458628+mandawilson@users.noreply.github.com>
2020-11-11 07:51:37 -0500

    bash script to remove redundant code

--- .github/k8s/curl.sh
+++ .github/k8s/curl.sh
@@ -1,26 +1,29 @@
 #!/bin/bash
 POD_NAME=$1
 MAX_TIME=$2
-STUDY_NAME=$3
-URL=$4
+URL=$3
+SEARCH_STR=$4
+POST_BODY=$5
 
 OK_RESPONSE_CODE=200
-EMPTY_LIST_RESPONSE=[]
-SEARCH_STR=entrezGeneId
+EMPTY_LIST_RESPONSE='[]'
 
-kubectl exec -it $POD_NAME --namespace=performance-test -- bash -c "curl --max-time $MAX_TIME -s -w '\n\n%{http_code}' -X POST '$URL' -H 'accept: application/json' -H 'Content-Type: application/json' -d '{\"studyIds\": [ \"$STUDY_NAME\" ]}'" > curl.out
+kubectl exec -it --namespace=performance-test $POD_NAME -- bash -c "curl --max-time $MAX_TIME -s -w '\n\n%{http_code}' -X POST '$URL' -H 'accept: application/json' -H 'Content-Type: application/json' -d '$POST_BODY'" > curl.out
 RESPONSE_CODE=`tail -1 curl.out`
 RESPONSE_BODY=`head -1 curl.out`
+echo "THIS RESPONSE BODY"
+echo "$RESPONSE_BODY"
+
 if [ "$RESPONSE_CODE" != "$OK_RESPONSE_CODE" ]; then
     echo "Request failed with response code '$RESPONSE_CODE', expected '$OK_RESPONSE_CODE'"
     exit 1
 else
-    if [ "$RESPONSE_BODY" == "\[\]" ] || [ -z "$RESPONSE_BODY" ]; then 
+    if [ "$RESPONSE_BODY" == "$EMPTY_LIST_RESPONSE" ] || [ -z "$RESPONSE_BODY" ]; then 
         echo "Request failed with empty response"
         exit 1
     else
-        if [ "$RESPONSE_BODY" != *"$SEARCH_STR"* ]; then
-            echo "Response did not contain search string '$SEARCH_STR'"
+        if [[ "$RESPONSE_BODY" != *"$SEARCH_STR"* ]]; then
+           echo "Response did not contain search string '$SEARCH_STR'"
             exit 1
         fi
     fi
